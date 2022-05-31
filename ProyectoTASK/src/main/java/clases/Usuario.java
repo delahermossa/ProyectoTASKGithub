d3f006a59e45e18a46f8eca8b1824d9d626de326
delahermossa <delahermossa@gmail.com>
2022-05-31 10:09:58 +0200

    commit

--- ProyectoTASK/src/main/java/clases/Usuario.java
+++ ProyectoTASK/src/main/java/clases/Usuario.java
@@ -13,7 +13,7 @@ public class Usuario {
 	private String apellidos;
 	private String contraseña;
 	private String direccion;
-	private Ciudad ciudad;/* Preguntar a Miguel si quitar direccion */
+	private Ciudad ciudad;
 	private boolean esAdmin;
 	private ArrayList<Servicio> serviciosOfrecidos;
 	private ArrayList<Cita> citasAgendadas;
